3ca94c295c68400b0ebb0fdd718ff5d86b6d2ba1
bloomlj <kollocc@live.com>
2018-05-07 07:22:59 +0800

    fix mongoose Deprecation
    DeprecationWarning: `open()` is deprecated in mongoose >= 4.11.0, use `openUri()` instead,

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const app = express();
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.openUri(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
